cef2e8fb7a84962f50b1b5a308c859d9a55dcbe5
Aingaran Thiru <aingaran@thiru.io>
2023-03-23 18:53:21 -0400

    Added morgan

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const morga = require("morgan")
 app.use(express.json());
+app.use(morga("dev"));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -10,6 +12,4 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.post("/", (req, res) => {
   console.log(req.body);
   res.send("Got a POST request");
-  
-
 })
