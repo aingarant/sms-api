4dccfa0f7e848611ef74434894a94fa25a9d00bd
Aingaran Thirunadarajah <aingaran@Aingarans-MacBook-Air.local>
2024-09-26 22:02:07 -0400

    update

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const morgan = require("morgan");
-const { MongoClient } = require("mongodb");
+const express = require("express")
+const app = express()
+const port = 3000
+const morgan = require("morgan")
+const { MongoClient } = require("mongodb")
 
-app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.json())
+app.use(morgan("dev"))
 
+require("dotenv").config()
+const uri = process.env.MONGO_URI
+const client = new MongoClient(uri, { useUnifiedTopology: true })
 
-require("dotenv").config();
-const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, { useUnifiedTopology: true });
+app.get("/", async (req, res) => res.send("Hello World!"))
 
-app.get("/", async(req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-app.post('/', async (req, res) => {
-
-
+app.post("/", async (req, res) => {
   try {
-    const { name, email } = req.body;
-    const db = client.db('freepbx');
+    // const { name, email } = req.body
+    const db = client.db("messages")
 
-    const newUser = req.body;
+    const newUser = req.body
 
-    const result = await db.collection('users').insertOne(newUser);
-    
+    const result = await db.collection("users").insertOne(newUser)
+
     if (result.acknowledged) {
-      res.status(201).json({ message: 'User created successfully', user: newUser });
+      res
+        .status(201)
+        .json({ message: "User created successfully", user: newUser })
     } else {
-      res.status(500).json({ message: 'Failed to create user' });
+      res.status(500).json({ message: "Failed to create user" })
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'An error occurred' });
+    console.error(error)
+    res.status(500).json({ message: "An error occurred" })
   }
-});
+})
